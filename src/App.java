c44d056eb0ed93f045d753708b9870b0856b5f8a
Pablo Feijo <pfeijo34@gmail.com>
2025-06-18 21:15:27 -0500

    Arreglo de bug al imprimir

--- src/App.java
+++ src/App.java
@@ -1,24 +1,18 @@
 import controllers.ArbolBinario;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
         ArbolBinario ab = new ArbolBinario();
 
         ab.insert(50);
         ab.insert(17);
         ab.insert(9);
         ab.insert(23);
-        ab.insert(14);
-        ab.insert(19);
         ab.insert(76);
         ab.insert(54);
 
-        ab.imprimirArbol(null);
-
-
-    
-    
+        ab.imprimirArbol();
+        ab.buscar(54);
     }
 
-    
 }
--- src/controllers/ArbolBinario.java
+++ src/controllers/ArbolBinario.java
@@ -20,30 +20,48 @@ public class ArbolBinario {
         }
         if (value < padre.getValue()) {
             //Me voy a la izquierda
-            padre.setIzquierda(insertRec(padre.getIzquierda(), value));
-            
+            padre.setIzquierda(insertRec(padre.getIzquierda(), value));            
         }else if (value > padre.getValue()) {
             //Me voy a la derecha
-            padre.setDerecha(insertRec(padre.getIzquierda(), value));
+            padre.setDerecha(insertRec(padre.getDerecha(), value));
         }
 
         return padre;
     }
     private void imprimir(Node nodo){
         if (nodo == null) {
-                    
+            return;   
         }
         if (nodo != null) {
                     
             imprimir(nodo.getIzquierda());
-            System.out.println(nodo.getValue() + ", ");
+            System.out.print(nodo.getValue() + ", ");
             imprimir(nodo.getDerecha());
         }
     }
 
     public void imprimirArbol(){
         imprimir(root);
-        
     }
+
+    public void buscar(int value){
+        boolean res = busquedaBinaria(root, value);
+        System.err.println(String.valueOf(res));
+    }
+
+    public boolean busquedaBinaria(Node nodo, int value){
+    if (nodo == null) {
+        return false;
+    }
+
+    if (nodo.getValue() == value){
+        return true;
+    } else if (value < nodo.getValue()) {
+        return busquedaBinaria(nodo.getIzquierda(), value);
+    } else {
+        return busquedaBinaria(nodo.getDerecha(), value);
+    }
+}
+
     
 }
